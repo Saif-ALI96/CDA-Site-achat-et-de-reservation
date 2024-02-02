994b87b5ab32d280d5a295dbed26641c8795d3ed
Saif Ali <kattan.saifali@gmail.com>
2024-02-02 14:08:39 +0100

    add: make all the route for the requetes sql to get the data

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const db = require("./db");
 // Second route to retrieve users from database
 app.get("/users", async (req, res) => {
   try {
-    console.log("La connexion à la base de données est bien établie.");
+    console.log("La connexion à la base de données Users est bien établie.");
     // Execute a SQL query to retrieve all users from the 'users' table
     const result = await db.query("SELECT * FROM users");
     // Respond with the retrieved users in JSON format
@@ -25,6 +25,74 @@ app.get("/users", async (req, res) => {
     // Log any errors that occur during the database query
     console.error(err);
     // Respond with a 500 Internal Server Error if an error occurs
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Third route to retrieve Notifications from database
+app.get("/Notifications", async (req, res) => {
+  try {
+    console.log(
+      "La connexion à la base de données Notifications est bien établie."
+    );
+    const result = await db.query("SELECT * FROM Notifications");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Fourth route to retrieve Vehicle from database
+app.get("/Vehicle", async (req, res) => {
+  try {
+    console.log("La connexion à la base de données Vehicle est bien établie.");
+    const result = await db.query("SElECT * FROM vehicle");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Fifth route to retrieve Reservation from database
+app.get("/Reservation", async (req, res) => {
+  try {
+    console.log(
+      "La connexion à la base de données Reservation est bien établie."
+    );
+    const result = await db.query("SElECT * FROM Reservation");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Sixth route to retrieve VehicleOption from database
+app.get("/VehicleOption", async (req, res) => {
+  try {
+    console.log(
+      "La connexion à la base de données VehicleOption est bien établie."
+    );
+    const result = await db.query("SElECT * FROM VehicleOption");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Seventh route to retrieve VehicleColor from database
+app.get("/VehicleColor", async (req, res) => {
+  try {
+    console.log(
+      "La connexion à la base de données VehicleColor est bien établie."
+    );
+    const result = await db.query("SElECT * FROM VehicleColor");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
     res.status(500).send("Internal Server Error");
   }
 });
